e9e6c157008089466129d39bcb85435d1065faa3
qimenlang <jiaxiangli@faceunity.com>
2020-07-21 22:44:14 +0800

    add glfw lib

--- Engine.cpp
+++ Engine.cpp
@@ -5,10 +5,9 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	return 0;
 }
