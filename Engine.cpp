64df1efaedb0710610610dd6f4a49459b8743132
qimenlang <jiaxiangli@faceunity.com>
2020-07-21 23:59:34 +0800

    create window

--- Engine.cpp
+++ Engine.cpp
@@ -3,11 +3,55 @@
 #include <GLFW/glfw3.h>
 using namespace std;
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) { 
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
 int main()
 {
+    //初始化glfw设置版本及核心模式
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    //使用glfw的核心模式，只能使用opengl的一个子集
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    //创建窗口并将窗口设置为当前线程的主上下文
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Engine", NULL, NULL);
+    if (window == NULL) {
+        cout << "Failed create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    //glad管理opengl函数地址，glfwGetProcAddress用于glfw加载opengl函数
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+      cout << "Failed to initialize GLAD" << endl;
+      return -1;
+    }
+
+    //创建视口，并为窗口设置调整视口的回调函数
+    glViewport(0, 0, 800, 600);
+    //glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    //渲染循环
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        //rendering
+        glClearColor(0.0f, 0.0f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 	return 0;
 }
